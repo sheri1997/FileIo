e04dbf2fb2b68cc079ea7b33c3cd3f669557cb30
Shreesh Pandey <shvspandey@gmail.com>
2021-12-09 15:43:23 +0530

    Displaying Welcome Message on the Console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the File IO Program");
+        }
+    }
+}
